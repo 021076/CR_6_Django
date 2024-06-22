1e9f5883349e4841968c8ca4977ad7db46812236
ro_k_ <ro_k_sana@mail.ru>
2024-06-22 16:05:21 +0500

    6 курс. Курсовая работа. Django. приложение users

---
+++ users/models.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='почта')
+    first_name = models.CharField(max_length=50, **NULLABLE, verbose_name='Имя')
+    last_name = models.CharField(max_length=50, **NULLABLE, verbose_name='Фамилия')
+    token = models.CharField(max_length=100, verbose_name='token', **NULLABLE)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        # permissions = [
+        #     ('view_user_list', 'Может просматривать список пользователей'),
+        #     ('block_users', 'Может блокировать пользователей')
+        # ]
